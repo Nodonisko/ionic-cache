4be38a057438d9a43b58d597c6083fd96f521f48
Nodonisko <suchydan@gmail.com>
2016-05-23 21:48:31 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,183 @@
-# ionic-cache
-Ionic cache service
+# Ionic 2 cache service
+
+My Ionic 2 cache service that can cache almost everything, include requests. It's made up to use WebSQL or SQLite 
+as storage and work well with Observables.
+
+**It's not well tested yet, so use it at your own risk.** Please report all bugs to bug report or fix it, or 
+better fix it and send pull request :)
+
+## Install
+
+Simple copy file to your providers folder and inject it in app.js file.
+
+```js
+import {CacheProvider} from "./providers/cache-provider/cache-provider";
+
+@App({
+    templateUrl: "build/app.html",
+    providers: [CacheProvider]
+})
+class MyApp {
+    static get parameters() {
+        return [[IonicApp], [Platform], [CacheProvider]];
+    }
+
+    constructor(app, platform, http, user, network, cache) {
+        ...
+        this.cache = cache;
+
+        this.cache.setTTL(20); //set default cache TTL for 1 minute
+        ....
+    }
+    ...
+```
+
+## Usage
+
+#### Cache request response body
+
+```js
+import {CacheProvider} from '../cache-provider/cache-provider';
+
+@Injectable()
+export class CategoryProvider {
+    static get parameters() {
+        return [[Http], [CacheProvider]]
+    }
+
+    constructor(http, cache) {
+        this.http = http;
+        this.cache = cache;
+    }
+
+    loadList() {
+        let url = "http://google.com";
+        let cacheKey = url;
+
+        let request = this.http.get(url).map(res => res.json());
+        return this.cache.loadItem(cacheKey, request);
+    }
+    ...
+```
+
+#### Cache whole request response
+
+Sometimes you need to cache whole response, if you need to look to Headers etc. It can be done only with simple 
+move .map(res => res.json()) after loadItem method. LoadItem returns Observable, so you can also use other 
+Observable operators
+
+```js
+...
+   let request = this.http.get(url);
+   return this.cache.loadItem(cacheKey, request).map(res => res.json());
+...
+```
+
+#### Cache with custom Observable operators
+
+LoadItem method using Observable and return Observable, so you can use lot of Observable operators. 
+For example error handling (on error, retry request every 6 seconds):
+
+```js
+...
+   let request = this.http.get(url)
+                        .retryWhen((error) => {
+                            return error.timer(6000);
+                        }).map(res => res.json());
+   return this.cache.loadItem(cacheKey, request);
+...
+```
+
+#### Cache entries grouping
+
+This is really nice feature. Sometimes you need to invalidate only some group of cache entries.
+For example if you have have long infinite scroll with lots of pages, and user trigger pull to request you want to delete
+all cache entries for all pages. So this is time for third parameter groupKey.
+
+```js
+...
+    loadList(pageNumber) {
+        let url = "http://google.com/?page=" + pageNumber;
+        let cacheKey = url;
+        let groupKey = "googleListPages"
+
+        let request = this.http.get(url).map(res => res.json());
+        return this.cache.loadItem(cacheKey, request, groupKey);
+    }
+...
+```
+
+And on pull to refresh delete all cache entries in group googleListPages:
+
+```js
+...
+    pullToRefresh() {
+        this.cache.removeByGroup("googleListPages");
+    }
+...
+```
+
+#### Set custom TTL for single request
+
+If you want custom TTL for single request, it can by easily done by third parameter.
+
+```js
+...
+    loadList(pageNumber) {
+        ...
+        let ttl = 60 * 60 * 24 * 7; // TTL in seconds for one week
+
+        let request = this.http.get(url).map(res => res.json());
+        return this.cache.loadItem(cacheKey, request, groupKey, ttl);
+    }
+...
+```
+
+#### Cache non-observables (arrays, strings etc.)
+
+This is not so smart, you must call getItem twice... It will be improved in upcoming versions.
+
+```js
+...
+   let arrayToCache = ["Hello", "World"];
+   let cacheKey = "my-array";
+
+   return new Promise((resolve, reject) => {
+        this.cache.getItem().then(item => {
+            resolve(item);
+        }).catch(() => {
+            this.cache.saveItem(cacheKey, arrayToCache).then(() => {
+                this.cache.getItem().then(item => {
+                    resolve(item);
+                });
+            });
+        });
+   });
+...
+```
+
+#### Delete expired entries
+
+It's automatically done on every startup, but you can do it manually.
+
+```js
+...
+    this.cache.removeExpired();
+...
+```
+
+#### Delete all entries
+
+```js
+...
+    this.cache.removeAll();
+...
+```
+
+#### Set default TTL
+
+```js
+...
+    this.cache.setTTL(60 * 60); //set default cache TTL for 1 hour
+...
+```
---
+++ cache-provider.js
@@ -0,0 +1,202 @@
+import {Injectable} from 'angular2/core';
+import {Storage, SqlStorage} from 'ionic-angular';
+import {Observable} from 'rxjs/Rx';
+import {Response, ResponseOptions} from 'angular2/http';
+
+@Injectable()
+export class CacheProvider {
+
+
+    static get parameters() {
+        return []
+    }
+
+    constructor() {
+        this.enableCache = true;
+        this.ttl = 60 * 60 * 1000; //one hour in ms
+        this.tableName = 'cache';
+        this.storage = new Storage(SqlStorage);
+
+        this.initDatabase().then(() => {
+            this.removeExpired();
+        });
+    }
+
+    /**
+     * @description Create DB table for cache, if not exists
+     * @return {Promise<T>}
+     */
+    initDatabase() {
+        let query = "CREATE TABLE IF NOT EXISTS " + this.tableName + " (key unique, value, expire, type, group_key)";
+
+        return this.storage.query(query);
+    }
+
+    /**
+     * @description Enables or disables cache
+     * @param {boolean} [status] - True/false
+     * @return {boolean}
+     */
+    enableCache(status = true) {
+        /* TODO */
+        return this.enableCache = status;
+    }
+
+    /**
+     * @description Set default TTL
+     * @param {number} ttl - TTL in seconds
+     * @return {number} new TTL time in miliseconds
+     */
+    setTTL(ttl) {
+        return this.ttl = ttl * 1000;
+    }
+
+    /**
+     * @description Save item to cache
+     * @param {string} key - Unique key
+     * @param {any} data - Data to store
+     * @param {string} [groupKey] - group key
+     * @param {number} [ttl] - TTL in seconds
+     * @return {Promise<T>} - query execution promise
+     */
+    saveItem(key, data, groupKey = 'none', ttl = this.ttl) {
+        let expireTime = new Date().getTime() + ttl;
+        let type = this.isRequest(data) ? 'request' : typeof data;
+        let value = JSON.stringify(data);
+
+        let query = "INSERT OR REPLACE INTO " + this.tableName + " (key, value, expire, type, group_key) VALUES ('" + key + "', '" + value + "', " + expireTime + ", '" + type + "', '" + groupKey + "')";
+
+        return this.storage.query(query).catch(err => console.error(err));
+    }
+
+    /**
+     * @description Delete item from cache
+     * @param {string} key - Unique key
+     * @return {Promise<T>} - query execution promise
+     */
+    removeItem(key) {
+        let query = "DELETE FROM " + this.tableName + " WHERE key = '" + key + "'";
+
+        return this.storage.query(query);
+    }
+
+    /**
+     * @description Get item from cache without expire check etx.
+     * @param {string} key - Unique key
+     * @return {Promise} - data from cache
+     */
+    getRawItem(key) {
+        let query = "SELECT * FROM " + this.tableName + " WHERE key = '" + key + "'";
+        return new Promise((resolve, reject) => {
+            this.storage.query(query).then(data => {
+                let rows = data.res.rows;
+                if (rows.length === 1) {
+                    resolve(rows.item(0));
+                } else {
+                    reject("No items found in database.");
+                }
+            }).catch(err => reject(err));
+        });
+    }
+
+    /**
+     * @description Get item from cache with expire check and correct type assign
+     * @param {string} key - Unique key
+     * @return {Promise<T>} - data from cache
+     */
+    getItem(key) {
+        return new Promise((resolve, reject) => {
+            this.getRawItem(key).then(data => {
+                if (data.expire < new Date().getTime()) {
+                    this.removeItem(key).then(() => {
+                        reject("Cache entry already expired");
+                    });
+                } else {
+                    let dataJson = JSON.parse(data.value);
+                    if (this.isRequest(dataJson)) {
+                        let requestOptions = new ResponseOptions({
+                            body: dataJson._body,
+                            status: dataJson.status,
+                            headers: dataJson.headers,
+                            statusText: dataJson.statusText,
+                            type: dataJson.type,
+                            url: dataJson.url
+                        });
+                        resolve(new Response(requestOptions));
+                    } else {
+                        resolve(dataJson);
+                    }
+                }
+            }).catch(err => {
+                reject(err)
+            });
+        });
+    }
+
+    /**
+     * @description Load item from cache if it's in cache or load from origin observable
+     * @param {string} key - Unique key
+     * @param {Observable} observable - Observable with data
+     * @param {string} [groupKey] - group key
+     * @param {number} [ttl] - TTL in seconds
+     * @return {Observable} - data from cache or origin observable
+     */
+    loadItem(key, observable, groupKey, ttl) {
+        observable = observable.share();
+        let data = Observable.fromPromise(this.getItem(key)).catch((e) => {
+            console.log(e, key);
+            observable.subscribe(res => this.saveItem(key, res, groupKey, ttl));
+            return observable;
+        });
+
+        return data;
+    }
+
+    /**
+     * Perform complete cache clear
+     * @return {Promise}
+     */
+    removeAll() {
+        let query = "DELETE FROM " + this.tableName;
+
+        return this.storage.query(query);
+    }
+
+    /**
+     * @description Remove all expired items from cache
+     * @return {Promise<T>} - query promise
+     */
+    removeExpired() {
+        let datetime = new Date().getTime();
+        let query = "DELETE FROM " + this.tableName + " WHERE expire < " + datetime;
+
+        return this.storage.query(query);
+    }
+
+    /**
+     * @description Remove all item with specified group
+     * @param {string} groupKey - group key
+     * @return {Promise<T>} - query promise
+     */
+    removeByGroup(groupKey) {
+        let datetime = new Date().getTime();
+        let query = "DELETE FROM " + this.tableName + " WHERE group_key = '" + groupKey + "'";
+
+        return this.storage.query(query);
+    }
+
+    /**
+     * @description Check if it's an request
+     * @param {any<T>} data - Variable to test
+     * @return {boolean} - data from cache
+     */
+    isRequest(data) {
+        if (typeof data == 'object' && data.hasOwnProperty('_body') && data.hasOwnProperty('status') &&
+            data.hasOwnProperty('statusText') && data.hasOwnProperty('type') && data.hasOwnProperty('headers')
+            && data.hasOwnProperty('url')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
