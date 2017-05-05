8860e28f10b7aa99d3adea719a84f99fcc7216fa
Ibby Hadeed <ibby93@gmail.com>
2017-05-05 06:53:04 -0400

    simplify test config

--- karma.conf.js
+++ karma.conf.js
@@ -3,70 +3,43 @@
 module.exports = config => {
 
   const conf = {
-    basePath: '',
-    frameworks: ['jasmine'],
+    frameworks: [
+      'jasmine',
+      'karma-typescript'
+    ],
 
     plugins: [
+      'karma-typescript',
       'karma-jasmine',
-      'karma-chrome-launcher',
       'karma-phantomjs-launcher',
-      'karma-jasmine-html-reporter',
-      'karma-babel-preprocessor',
+      'karma-chrome-launcher'
     ],
 
     preprocessors: {
-      'dist/**/*.js': ['babel']
+      'src/**/*.ts': ['karma-typescript']
     },
 
-    babelPreprocessor: {
-      options: {
-        presets: ['es2015'],
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        entrypoints: /\.spec\.ts$/,
+        transforms: [
+          require("karma-typescript-angular2-transform")
+        ]
+      },
+      compilerOptions: {
+        lib: ['es2015', 'dom']
+      },
+      coverageOptions: {
+        instrumentation: true
       }
     },
 
     files: [
-      'node_modules/babel-polyfill/dist/polyfill.js',
-      'node_modules/systemjs/dist/system.src.js',
-
-      // Polyfills
-      'node_modules/core-js/client/shim.js',
-
-      // zone.js
-      'node_modules/zone.js/dist/zone.js',
-      'node_modules/zone.js/dist/long-stack-trace-zone.js',
-      'node_modules/zone.js/dist/proxy.js',
-      'node_modules/zone.js/dist/sync-test.js',
-      'node_modules/zone.js/dist/jasmine-patch.js',
-      'node_modules/zone.js/dist/async-test.js',
-      'node_modules/zone.js/dist/fake-async-test.js',
-
-      {pattern: 'karma-test-shim.js', included: true, watched: true},
-
-      // paths loaded via module imports
-      {pattern: 'dist/**/*.js', included: false, watched: true},
-
-      // paths to support debugging with source maps in dev tools
-      {pattern: 'src/**/*.ts', included: false, watched: false},
-      {pattern: 'dist/**/*.js.map', included: false, watched: false},
-
-      // RxJs
-      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
-
-      // Paths loaded via module imports:
-      // Angular itself
-      { pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
-      { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
-
-      { pattern: 'systemjs.config.js', included: false, watched: false }
+      'src/cache.service.spec.ts',
+      { pattern: 'src/**/*', included: true, watched: true },
     ],
 
-    // Proxied base paths for loading assets
-    proxies: {
-      '/dist/': '/base/dist/'
-    },
-    exclude: [],
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'karma-typescript'],
 
     port: 9876,
     colors: true,
--- src/cache.service.spec.ts
+++ src/cache.service.spec.ts
@@ -1,17 +1,30 @@
+import "core-js"
+import "zone.js/dist/zone";
+import "zone.js/dist/long-stack-trace-zone";
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+
 import { CacheService, MESSAGES } from './cache.service';
-import { async } from '@angular/core/testing';
+import { async, TestBed, inject } from '@angular/core/testing';
+import {Http, HttpModule, XHRBackend, Response, ResponseOptions, RequestMethod} from "@angular/http";
+import {MockBackend, MockConnection} from '@angular/http/testing';
+
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
 describe('CacheService', () => {
 
-  let service: CacheService;
+  let service: any;
 
   beforeAll(() => {
     service = new CacheService();
   });
 
-  it('should create an instance of the service', () => {
-    expect(service).toBeDefined();
-  });
+  it('should create an instance of the service', () => expect(service).toBeDefined());
 
   it('should save item to storage (async)', async(() => {
     service.saveItem('name', 'ibby')
@@ -35,5 +48,57 @@ describe('CacheService', () => {
       .then(() => expect(false).toBeTruthy())
       .catch((e) => expect(e).toEqual(MESSAGES[1]));
   }));
+
+  it('should enable cache', () => {
+    service.enableCache(true);
+    expect((<any> service).cacheEnabled === true).toBeTruthy();
+  });
+
+});
+
+describe('Http Caching', () => {
+
+  const key: string = 'http_cache_test';
+
+  const mockUrl: string = 'https://somelink.com/path/to/endpoint';
+
+  const mockResponse = {
+    hello: 'world'
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpModule ],
+      providers: [
+        { provide: XHRBackend, useClass: MockBackend },
+        CacheService
+      ]
+    });
+  });
+
+  it('should create an instance of the service', inject([XHRBackend, CacheService], (mockBackend: XHRBackend, cache: CacheService) => {
+    expect(mockBackend).toBeDefined();
+    expect(cache).toBeDefined();
+  }));
+
+  // it('', inject([XHRBackend, CacheService], (mockBackend: XHRBackend, cache: CacheService) => { }))
+
+  it('should cache HTTP request', async(inject([XHRBackend, CacheService, Http], (mockBackend: MockBackend, cache: CacheService, http: Http) => {
+
+    mockBackend.connections.subscribe((connection: MockConnection) => {
+      expect(connection.request.method).toBe(RequestMethod.Get);
+      expect(connection.request.url).toBe(mockUrl);
+
+      connection.mockRespond(new Response(
+        new ResponseOptions({ body: mockResponse })
+      ));
+    });
+
+    cache.loadFromObservable(key, http.get(mockUrl))
+      .subscribe(res => {
+        expect(res).toEqual(mockResponse);
+      });
+
+  })));
 
 });
